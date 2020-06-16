d4761db1f4a933d61adece29856ebc8bb45bf7c7
Ryan Mills <millselonius@gmail.com>
2020-06-16 08:10:18 -0600

    bloom attempt

--- stars.js
+++ stars.js
@@ -1,21 +1,20 @@
+//Scene
 var scene = new THREE.Scene();
 
+//Camera
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 10;
 
+//Renderer
 var renderer = new THREE.WebGLRenderer({antialias: true}); 
 renderer.setSize(window.innerWidth, window.innerHeight); document.body.appendChild(renderer.domElement);
 
+//Textures
 var starTexture1 = new THREE.TextureLoader().load("dyingstar.png");
 var starTexture2 = new THREE.TextureLoader().load("bluestar.png");
 var starTexture3 = new THREE.TextureLoader().load("dwarfstar.png");
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
-
+//Stars
 var stars1 = [];
 
 for (let i = 0; i < 150; i++) {
@@ -61,8 +60,30 @@ for (let j = 0; j < stars3.length; j++) {
   scene.add( stars3[j] );
 }
 
+//Randomization
 function getRandom() {
   var num = Math.floor(Math.random()*10) + 1;
   num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
   return num;
 }
+
+//Composer
+var composer = new THREE.EffectComposer(renderer);
+
+//Passes
+var renderPass = new THREE.RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+var pass1 = new THREE.BloomPass();
+composer.addPass(pass1);
+pass1.renderToScreen = true;
+
+//Animation/Render
+render();
+
+function render() {
+  composer.render();
+  requestAnimationFrame(render);
+}
+
+render();
